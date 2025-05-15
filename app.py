7d62135919ea55881bdd1d9074cf3b437fd80f48
Prajakta-27-pck <praj.kante01@gmail.com>
2025-05-15 05:30:15 +0000

    first commit

--- app.py
+++ app.py
@@ -15,3 +15,25 @@ st.title("Stock Market app")
 # add subheading
 
 st.subheader("By Prajakta Kante")
+
+
+## add text box for serching company
+
+company = st.text_input("Company Name")
+
+
+## create function for making connection between stockapi class and app
+
+@st.cache_resource(ttl= 3600)
+def fetch_data():
+    return StockApi()
+
+
+## create function for getting symbol
+
+
+
+
+
+
+
