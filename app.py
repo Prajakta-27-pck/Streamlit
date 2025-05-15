feed6f65993e9355f4036045b0d370fc0ff72b2f
Prajakta-27-pck <praj.kante01@gmail.com>
2025-05-15 05:12:59 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from client import StockApi
+
+
+# Create page title
+
+st.set_page_config("Stock Market app")
+
+
+# add title
+
+st.title("Stock Market app")
+
+
+# add subheading
+
+st.subheader("By Prajakta Kante")
